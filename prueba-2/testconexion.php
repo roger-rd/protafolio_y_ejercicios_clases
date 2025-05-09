831444acef7f65539e7aafa51d1e46536b8a3c55
Roger Rodriguez <110642845+roger-rd@users.noreply.github.com>
2025-05-09 10:43:53 -0400

    codigo comentado testconexion.php

--- prueba-2/testconexion.php
+++ prueba-2/testconexion.php
@@ -1,3 +1,6 @@
+// Este archivo prueba la conexión a la base de datos.
+// Si la conexión se establece correctamente a través del archivo 'conexion.php',
+// se muestra el mensaje "Conexión exitosa a la base de datos!".
 <?php
 require_once 'conexion.php';
 echo "Conexión exitosa a la base de datos!";
